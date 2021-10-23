75b3d8a5577ab615b6ff88affab0bdfb881b2739
Agus Supriyatna <aguzsupriyatna7@gmail.com>
2021-10-23 16:39:28 +0700

    soal 3

---
+++ soal_3_agus_supriyatna.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+print("Soal 2 - Agus Supriyatna")
+print("Program perhitungan data nilai\n")
+
+MAX_INPUT = 3
+
+value_lists = []
+value_sum   = 0
+
+for i in range(1, MAX_INPUT + 1):
+    dummy_input = int(input('Masukkan nilai ke-{:d}: '.format(i)))
+
+    value_lists.append(dummy_input)
+    value_sum += dummy_input
+
+print()
+print('Nilai yang diinput adalah', ', '.join(map(str, value_lists)))
+print('Total nilai yang diinput adalah', value_sum)
+print('Rata-rata nilai yang diinput adalah', value_sum / MAX_INPUT)
